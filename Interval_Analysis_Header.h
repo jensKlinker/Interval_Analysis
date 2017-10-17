30d31ff35ba193f11a077ee64e029ca5369a0bcc
jens <jens.klinker@peyclon.de>
2017-10-17 13:41:49 +1300

    Minor fixes to infrastructure

--- Interval_Analysis_Header.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTERVAL_ANALYSIS_HEADER_H_INCLUDED
-#define INTERVAL_ANALYSIS_HEADER_H_INCLUDED
-
-#include <iostream>
-#include <boost/numeric/interval.hpp>
-
-int test();
-
-#endif // INTERVAL_ANALYSIS_HEADER_H_INCLUDED
--- src/I_A_Header.h
+++
@@ -1,9 +0,0 @@
-#ifndef I_A_HEADER_H    // To make sure you don't declare the function more than once by including the header multiple times.
-#define I_A_HEADER_H
-
-#include <iostream>
-#include <boost/numeric/interval.hpp>
-
-int test();
-
-#endif
